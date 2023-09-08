52d05d360748d843f0e93939273a84ebc95a2a9d
aapsi <aapsi0001@gmail.com>
2023-09-08 17:10:09 +0530

    vending machine app

---
+++ pages/vending-machine.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import 'bulma/css/bulma.css';
+import styles from '../styles/vendingMachine.module.css';
+
+
+export default function VendingMachine() {
+    return (
+        <div>
+            <Head>
+                <title>Vending Machine App</title>
+                <meta />
+            </Head>
+            <h1>Vending Machine</h1>
+        </div>
+        
+    )
+}
