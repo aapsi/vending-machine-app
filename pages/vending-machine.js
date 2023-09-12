dc501a1035d1d4c9f6fd58fc41f82b51dd45b572
aapsi <aapsi0001@gmail.com>
2023-09-13 00:57:27 +0530

    changing font

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import Web3 from 'web3';
 import 'bulma/css/bulma.css';
 import styles from '../styles/vendingMachine.module.css';
@@ -11,16 +12,33 @@ const VendingMachine = () => {
     // allows user to sign messages and transactions
     // allows us to read data from blockchain
 
-    // window.ethereum
-    const connectWalletHandler = () => {
-        alert('connect wallet');
+    // window.ethereum -> this object is only going to exist if user has installed metamask
+    
+    const [error, setError] = useState('');
+    let web3;
+    
+    const connectWalletHandler = async () => {
+        if(typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+            // we will first request the accounts to check if the connection is established
+            try{
+                window.ethereum.request({ method: "eth_requestAccounts"})
+                web3 = new Web3()
+            } catch(err) {
+                setError(err.message)
+            }
+        
+            
+        } else {
+            console.log("Please install Metamask")
+        }
     }
     return (
         <div className={styles.main}>
-            <Head>
-                <title>Vending Machine App</title>
-                <meta name= 'description' content='A blockchain vending machine app'/>
-            </Head>
+        <Head>
+            <title>Vending Machine App</title>
+            <meta name="description" content="A blockchain vending machine app" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" />
+        </Head>
             <nav className='navbar mt-4 mb-4'>
                 <div className='container'>
                     <div className='navbar-brand'>
@@ -36,6 +54,12 @@ const VendingMachine = () => {
                 {/* className='container'-> bulma convention to give some padding and spacing */}
                 <div className='container'>
                     <p>placeholder text</p>
+                </div>
+            </section>
+            <section>
+                {/* className='container'-> bulma convention to give some padding and spacing */}
+                <div className='container has-text-danger'>
+                    <p>{error}</p>
                 </div>
             </section>
         </div>
