cab72b15e090c991fb9f8483be81f102c52b98a4
aapsi <aapsi0001@gmail.com>
2023-09-19 00:44:36 +0530

    smart contract

---
+++ blockchain/vending.js
@@ -0,0 +1,12 @@
+// the sole purpose of this file is to export a local copy of our smart contract
+import Web3 from 'web3';
+
+const provider = new Web3.providers.HttpProvider(
+    "https://sepolia.infura.io/v3/a1c4b346baee4857b15d643b84f08920"
+)
+
+const web3 = new Web3(provider)
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vmContract = new web3.eth.Contract(abi, "0xA5Bb567A147020Db4c6D1f11469b586dDFbA9218")
